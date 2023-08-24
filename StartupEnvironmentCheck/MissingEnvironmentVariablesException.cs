82b5b95d03cc5d4b78cb25faaed5b5bec6992024
Haik <haik.asatryan.95@gmail.com>
2023-08-25 00:26:12 +0400

    refactore exception message

--- StartupEnvironmentCheck/MissingEnvironmentVariablesException.cs
+++ StartupEnvironmentCheck/MissingEnvironmentVariablesException.cs
@@ -9,8 +9,8 @@ public class MissingEnvironmentVariablesException : Exception
 
     private static string GenerateMessage(IEnumerable<string> missingVariables)
     {
-        var joinedNames = string.Join(", ", missingVariables);
+        var joinedNames = string.Join("\n", missingVariables.Select(v => $"- {v}"));
         return
-            $"The following environment variable(s) are missing: `{joinedNames}`. Please ensure that these variables are properly configured in the environment settings. These variables might be essential for connecting to specific services or performing necessary application functions.";
+            $"The following environment variable(s) are missing:\n{joinedNames}\nPlease ensure that these variables are properly configured in the environment settings. These variables might be essential for connecting to specific services or performing necessary application functions.";
     }
 }
